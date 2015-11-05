a1e273d0a09f87f6cca6fe83a5eaafa8b78c482d
Jeremy Greenwood <jeremy.rmrr@gmail.com>
2015-11-05 15:52:56 -0800

    Upload image and caption

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,21 @@ class PostsController < ApplicationController
 
 	def index
 	end
+
+	def new
+		@post = Post.new
+		redirect_to posts_path
+	end
+
+	def create 
+		@post = Post.create(post_params)
+		redirect_to posts_path
+
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:image, :caption)
+	end
 end

27f12c321650c3a955bd9c9427d892a29a08d149
Jeremy Greenwood <jeremy.rmrr@gmail.com>
2015-11-09 13:49:25 -0800

    Add partial for new and edit form views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,13 +18,18 @@ class PostsController < ApplicationController
 	end
 
 	def edit
-		@post = Post.find(params[id])
+		@post = Post.find(params[:id])
 	end
 
 	def update
-		@post = User.find(params[id])
+		@post = Post.find(params[:id])
 	  @post.update(post_params)
 		redirect_to(post_path(@post))
+	end
+
+	def destroy
+		@post = Post.find(params[:id]).destroy
+		redirect_to posts_path
 	end
 
 	private

4741b12f4597ca193c8894e90fcde7aa2ed68ee9
Jeremy Greenwood <jeremy.rmrr@gmail.com>
2015-11-09 12:55:49 -0800

    Add destroy action and corresponding delete method in edit view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,16 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 
+	def edit
+		@post = Post.find(params[id])
+	end
+
+	def update
+		@post = User.find(params[id])
+	  @post.update(post_params)
+		redirect_to(post_path(@post))
+	end
+
 	private
 
 	def post_params
